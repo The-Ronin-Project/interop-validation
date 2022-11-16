5a64a1f2e0955e8958ec194e90244046a35fa2e6
Josh Smith <43484632+arssycro@users.noreply.github.com>
2022-11-16 17:57:29 -0600

    INT-1221: Upgrade to interop-gradle 2.2.0 (#76)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,14 +10,14 @@ for (project in rootProject.children) {
 
 pluginManagement {
     plugins {
-        id("com.projectronin.interop.gradle.docker-integration") version "2.1.4"
-        id("com.projectronin.interop.gradle.integration") version "2.1.4"
-        id("com.projectronin.interop.gradle.jacoco") version "2.1.4"
-        id("com.projectronin.interop.gradle.junit") version "2.1.4"
-        id("com.projectronin.interop.gradle.publish") version "2.1.4"
-        id("com.projectronin.interop.gradle.spring") version "2.1.4"
-        id("com.projectronin.interop.gradle.spring-boot") version "2.1.4"
-        id("com.projectronin.interop.gradle.version") version "2.1.4"
+        id("com.projectronin.interop.gradle.docker-integration") version "2.2.0"
+        id("com.projectronin.interop.gradle.integration") version "2.2.0"
+        id("com.projectronin.interop.gradle.jacoco") version "2.2.0"
+        id("com.projectronin.interop.gradle.junit") version "2.2.0"
+        id("com.projectronin.interop.gradle.publish") version "2.2.0"
+        id("com.projectronin.interop.gradle.spring") version "2.2.0"
+        id("com.projectronin.interop.gradle.spring-boot") version "2.2.0"
+        id("com.projectronin.interop.gradle.version") version "2.2.0"
 
         id("org.jetbrains.gradle.plugin.idea-ext") version "1.1.6"
         id("org.openapi.generator") version "6.2.1"
